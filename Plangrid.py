3f6c9ceafd396616a7d2084166d04b9cd9af4f76
Michael Rice <MichaelRi@sellen.com>
2019-08-14 08:31:25 -0700

    Oauth setup testing

--- Plangrid.py
+++ Plangrid.py
@@ -3,9 +3,13 @@ import requests, configparser, os
 config = configparser.ConfigParser()
 config.readfp(open('config.txt'))
 
-API = "https://io.plangrid.com/"
+API = "https://io.plangrid.com"
+OauthURL = "/oath/authorize"
+TokenURL = "/oath/token"
 API_Key = config.get('Default', 'Key')
-Headers = {'Accept': 'application/vnd.plangrid+json'}
+Client_ID = config.get('Default', 'ClientID')
+Client_Secret = config.get('Default', 'ClientSecret')
+Headers = {'Accept': 'application/vnd.plangrid+json; version=1'}
 
-r = requests.get(API, auth=(API_Key, ''), headers=Headers)
+r = requests.get(API + OauthURL + '?response_type=code&client_id=' + Client_ID + '&state=1&scope=write:projects')
 print(r.text)
