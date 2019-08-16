2afdb440f778f12fcc41070b6afe142276181449
Michael Rice <MichaelRi@sellen.com>
2019-08-16 14:17:25 -0700

    Prepare for live demo

--- Plangrid.py
+++ Plangrid.py
@@ -1,71 +1,12 @@
-import requests, configparser, os, json
+import Demo1, Demo2
 
-# Initialize ConfigParser
-config = configparser.ConfigParser()
-config.readfp(open('config.txt'))
+switcher = {
+    1: Demo1,
+    2: Demo2
+}
 
-# Load data from config.txt
-Client_ID = config.get('Default', 'ClientID')
-Client_Secret = config.get('Default', 'ClientSecret')
+print("Which demo would you like to run")
+print(list(switcher.keys()))
+demonum = input()
 
-# Set default variables
-API = "https://io.plangrid.com"
-RequestAccessTokenHeader = {'Content-Type': 'application/x-www-form-urlencoded'}
-DemoMode = 'apikey'
-
-# Request that the user authorize this application and provide the authorization code
-def GetTokens():
-    print("Navigate to the following URL, sign in to Plangrid, and give permission to access your account. Once permission has been granted, please paste the code below")
-    print(API + '/oauth/authorize' + '?response_type=code&client_id=' + Client_ID + '&state=1&scope=write:projects')
-    print("Code:")
-    Authorization_Code = input()
-
-    # Use the provided authorization code to obtain the access token and refresh token
-    if len(Authorization_Code) > 0:
-        r = requests.post(API + '/oauth/token', headers = RequestAccessTokenHeader, data = {
-            'client_id': Client_ID,
-            'client_secret': Client_Secret,
-            'grant_type': 'authorization_code',
-            'code': Authorization_Code,
-        })
-        j = json.loads(r.text)
-
-        # If the key 'error' is found in the json, show the error message. Otherwise load the tokens into the config file
-        if 'error' in j:
-            print("An error has occurred: " + j['error'])
-        elif 'access_token' in j and 'refresh_token' in j:
-            config['Default']['access_token'] = j['access_token']
-            config['Default']['refresh_token'] = j['refresh_token']
-            with open('config.txt', 'w') as configfile:
-                config.write(configfile)
-            print("Oauth token saved successfully")
-        else:
-            print("No token was retrieved")
-    else:
-        print("No code provided")
-
-# Use collected token to run demonstration
-def RunDemo(mode):
-    r = ''
-    if mode == 'oauth':
-        Header = {
-            'Authorization': 'Bearer ' + config.get('Default', 'access_token'),
-            'Accept': 'application/vnd.plangrid+json; version=1'
-        }
-
-        r = requests.get(API + '/projects', headers = Header)
-    elif mode == 'apikey':
-        Header = {
-            'Accept': 'application/vnd.plangrid+json; version=1'
-        }
-
-        r = requests.get(API + '/projects', headers = Header, auth = (config.get('Default', 'key'), ''))
-    
-    if r != '':
-        j = json.loads(r.text)
-        print(json.dumps(j, indent=4, sort_keys=True))        
-
-# If the token has not been obtained, get it. Run the demo using the token.
-if DemoMode == 'oauth' and len(config.get('Default', 'access_token')) == 0:
-    GetTokens()
-RunDemo(DemoMode)
+switcher.get(int(demonum)).RunDemo()
