ccc1644e103bb1b71b63bd3f27018c0e5bcc436d
Michael Rice <MichaelRi@sellen.com>
2019-08-19 08:45:41 -0700

    Remove demo and begin outputting CSV data

--- Plangrid.py
+++ Plangrid.py
@@ -1,4 +1,6 @@
-import requests, configparser, os, json
+import requests, configparser, json, csv
+import dateutil.parser
+from datetime import datetime
 
 # Initialize ConfigParser
 config = configparser.ConfigParser()
@@ -8,7 +10,22 @@ config.readfp(open('config.txt'))
 API = "https://io.plangrid.com"
 auth = (config.get('Default', 'key'), '')
 
-def RunDemo():
+def GetTasks():
+    csv_file  = open('plangrid_tasks.csv', 'w', newline='', encoding='utf-8')
+    csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+    csv_writer.writerow([
+        'PROJECT',
+        'DESCRIPTION',
+        'TYPE',
+        'DATE_CREATED',
+        'ROOT_CAUSE',
+        'DIVISION',
+        'CAUSED_BY',
+        'IDENTIFIED_BY',
+        'ESTIMATED_VALUE',
+        'MD_NATURAL_KEY'
+    ])
+    
     Header = {
          'Accept': 'application/vnd.plangrid+json; version=1'
     }
@@ -21,4 +38,20 @@ def RunDemo():
         issuereq = requests.get(API + '/projects/' + project['uid'] + '/issues?include_annotationless=true', headers = Header, auth = auth)
         issues = json.loads(issuereq.text)
         for issue in issues['data']:
+            types = ''
             print('    ' + str(issue['number']) + ' - ' + issue['title'])
+            if issue['issue_list'] != None:
+                issue_types = requests.get(issue['issue_list']['url'], headers = Header, auth = auth)
+                types = json.loads(issue_types.text)['name']
+            else:
+                types = 'General'
+            csv_writer.writerow([
+                project['name'],
+                issue['title'],
+                types,
+                dateutil.parser.parse(issue['created_at']).strftime("%m/%d/%Y %H:%M")
+            ])
+
+if __name__ == "__main__":
+    print("Saving tasks to CSV")
+    GetTasks()
