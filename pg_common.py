6cd5468451e4a46f71eb5cb157e03c7bd9c8abe9
Michael Rice <MichaelRi@sellen.com>
2019-08-23 07:24:09 -0700

    Create a standard pretty print function for JSON

--- pg_common.py
+++ pg_common.py
@@ -4,6 +4,7 @@ import csv # Required to output to CSV files
 import dateutil.parser # Required to read the date in ISO-8601 format
 from dateutil.tz import * # Required to shift the time from UTC to local timezone
 from datetime import datetime # Required to output the date/time in the expected format
+import json # Required to pretty print JSON data for easier readability
 
 # Set the root URL for the Plangrid API
 RootURL = "https://io.plangrid.com"
@@ -21,6 +22,12 @@ SampleProjects = [
 #formats the time as MM/DD/YYYY H:M
 def DateTime(sourcetime):
     return dateutil.parser.parse(sourcetime).astimezone(tzlocal()).strftime("%m/%d/%Y %H:%M")
+
+# Takes unformatted JSON text and formats it using a standard format. The result is JSON
+#data that is much easier to read, split out over multiple lines instead of crammed
+#together on a single line
+def PrettyPrint(JSON):
+    return json.dumps(JSON, indent = 4, sort_keys = True)
 
 # Manages safe writing to a CSV file
 class CSV:
