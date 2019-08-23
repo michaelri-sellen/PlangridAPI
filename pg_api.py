ff45fbbef8a135c2e43c73233be8d83d582d433f
Michael Rice <MichaelRi@sellen.com>
2019-08-23 07:23:41 -0700

    Correct API module documentation

--- pg_api.py
+++ pg_api.py
@@ -3,14 +3,14 @@
 
 import requests # Required to make the API call
 import configparser # Required to load the API key from config.txt
-import json # Required to format API responses as JSON
+import json # Required to format JSON API responses as Python objects
 
 # Initialize ConfigParser and open config.txt
 config = configparser.ConfigParser()
 config.readfp(open('config.txt'))
 
-# Set authentication to the API key from config.txt and headers to the default
-#headers expected by the Plangrid API
+# Set auth to the API key from config.txt and headers to the default headers 
+#expected by the Plangrid API
 auth = (config.get('Default', 'key'), '')
 headers = {'Accept': 'application/vnd.plangrid+json; version=1'}
 
